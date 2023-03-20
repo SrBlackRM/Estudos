d8da8d94c5d37712ecb35b23d60c07d380ec44ba
SrBlackRM <michelrbm@gmail.com>
2023-03-20 11:48:38 -0300

    Added 04 - Express

---
+++ javascript/js-node/04 - express/README.md
@@ -0,0 +1,3 @@
+# Express
+
+O Express é um pacote que melhora o gerenciamento do servidor node, pode ser usado para criação de API, encaminhamento de Rotas
---
+++ javascript/js-node/04 - express/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+// http://meusite.com/ <- GET -> entregue a página
+
+
+//         criar   ler   editar  apagar
+// CRUD -> CREATE, READ, UPDATE, DELETE
+//         POST    GET   PUT     DELETE
